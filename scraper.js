9a3c12cdcf44da13d3b5a79358a2e084275ad660
ryankuah <ryk326@lehigh.edu>
2024-08-06 12:03:33 +1000

    scraped most fields from mhpstore

--- scraper.js
+++ scraper.js
@@ -5,11 +5,78 @@ const process = require("process");
 const { authenticate } = require("@google-cloud/local-auth");
 const { google } = require("googleapis");
 
-function MobileHomePark(name, address, price) {
-  this.name = name;
-  this.address = address;
-  this.price = price;
+function MobileHomePark() {
+  this.name = null;
+  this.link = null;
+  this.address = null;
+  this.price = null;
+  this.broker = null;
+  this.brokerFirm = null;
+  this.desc = null;
+  this.locDesc = null;
+  this.occupancy = null;
+  this.lots = null;
+  this.yearBuilt = null;
+  this.size = null;
+  this.lotRent = null;
+  this.communityType = null;
+  this.water = null;
+  this.waterPaid = null;
+  this.sewer = null;
+  this.grossIncome = null;
+  this.operatingExpense = null;
+  this.operatingIncome = null;
+  this.infoType = null;
+  this.capRate = null;
+  this.debtInformation = null;
+  this.sgLots = null;
+  this.dbLots = null;
+  this.tpLots = null;
+  this.pmLots = null;
+  this.parkOwned = null;
+  this.avgRent = null;
+  this.rvLots = null;
+  this.rvLotRent = null;
 }
+
+function ParkData(desc, data) {
+  this.desc = desc;
+  this.data = data;
+}
+
+//#search-results > div.search-results-list > div:nth-child(3) > nav > ul > li:nth-child(6) > a
+
+(async () => {
+  for (let i = 1; i <= 13; i++) {
+    const pageLinks = await scrape(
+      `https://www.mobilehomeparkstore.com/mobile-home-parks-for-sale/usa/page/${i}`,
+    );
+    const parkResPromises = pageLinks.map((link) => scrapeListing(link));
+    var parkRes = await Promise.all(parkResPromises);
+    parkRes = parkRes.filter((res) => res !== null);
+
+    try {
+      const auth = await authorize();
+      await updateSpreadsheet(auth, parkRes);
+    } catch (error) {
+      console.error(error);
+    }
+  }
+})();
+
+/*
+(async () => {
+  var temp = await scrapeListing(
+    "https://www.mobilehomeparkstore.com/mobile-home-parks/6677277-3-500-pad-manufactured-home-community-selling-5-equity-for-sale-in-southport-fl",
+  );
+
+  var tempArr = [];
+  tempArr.push(temp);
+  authorize()
+    .then((auth) => updateSpreadsheet(auth, tempArr))
+    .catch(console.error);
+})();
+*/
 
 const SCOPES = ["https://www.googleapis.com/auth/spreadsheets"];
 const TOKEN_PATH = path.join(process.cwd(), "token.json");
@@ -69,6 +136,8 @@ async function scrape(URL) {
 async function scrapeListing(URL) {
   const browser = await puppeteer.launch();
   const page = await browser.newPage();
+  let park = new MobileHomePark();
+  park.link = URL;
   await page.goto(URL, { timeout: 120000 });
 
   await page.waitForSelector(".page-header");
@@ -80,28 +149,63 @@ async function scrapeListing(URL) {
     const nameElement = headerElement.querySelector('[itemprop="name"]');
     return nameElement ? nameElement.innerText : null;
   });
+  park.name = nameLocation.split("\n")[0];
+  park.address = nameLocation.split("\n")[1].trim();
 
-  const price = await page.evaluate(() => {
+  park.price = await page.evaluate(() => {
     const priceElement = document.querySelector(
       "#main > div.container > div.page-header.bordered.mb0 > div.row > div.col-fixed.text-right > h1",
     );
     return priceElement ? priceElement.innerText : null;
   });
 
-  await browser.close();
-
-  if (nameLocation && price) {
-    const park = new MobileHomePark(
-      nameLocation.split("\n")[0],
-      nameLocation.split("\n")[1].trim(),
-      "$" + price.split("$")[1],
+  park.broker = await page.evaluate(() => {
+    const brokerElement = document.querySelector(
+      "#sidebar > div > div.card.shadow > h2",
     );
-    console.log(park);
+    return brokerElement ? brokerElement.innerText : null;
+  });
+
+  park.brokerFirm = await page.evaluate(() => {
+    const brokerFirmElement = document.querySelector(
+      "#sidebar > div > div.card.shadow > h2 > div > div",
+    );
+    return brokerFirmElement ? brokerFirmElement.innerText : null;
+  });
+
+  const dataRows = await page.evaluate(() => {
+    var results = [];
+    try {
+      var trHtml = document
+        .querySelector(
+          "#content > div.row.justify-content-md-center > div.col-fluid > div > div:nth-child(3)",
+        )
+        .querySelectorAll("tr");
+      trHtml.forEach((tr) => {
+        var data = tr.querySelectorAll("td");
+        results.push(data[0].innerText, data[1].innerText);
+      });
+      return results;
+    } catch (err) {
+      console.log(err);
+      return null;
+    }
+  });
+  if (!dataRows) {
     return park;
-  } else {
-    console.log("Required elements not found on the page.");
-    return null;
   }
+
+  await browser.close();
+  let parkData = [];
+  for (let i = 0; i < dataRows.length; i += 2) {
+    parkData.push(new ParkData(dataRows[i], dataRows[i + 1]));
+  }
+  console.log(parkData);
+
+  setParkData(park, parkData);
+  console.log(park);
+
+  return park;
 }
 
 async function findNextEmptyRow(auth) {
@@ -117,10 +221,42 @@ async function findNextEmptyRow(auth) {
 async function updateSpreadsheet(auth, park) {
   console.log("this" + park);
   const index = await findNextEmptyRow(auth);
-  const range = `A${index}:C${index + park.length - 1}`;
+  const range = `A${index}:AE${index + park.length - 1}`;
   const _values = [];
   await park.forEach((park) => {
-    _values.push([park.name, park.address, park.price]);
+    _values.push([
+      park.name,
+      park.link,
+      park.address,
+      park.price,
+      park.broker,
+      park.brokerFirm,
+      park.desc,
+      park.locDesc,
+      park.occupancy,
+      park.lots,
+      park.yearBuilt,
+      park.size,
+      park.lotRent,
+      park.communityType,
+      park.water,
+      park.waterPaid,
+      park.sewer,
+      park.grossIncome,
+      park.operatingExpense,
+      park.operatingIncome,
+      park.infoType,
+      park.capRate,
+      park.debtInformation,
+      park.sgLots,
+      park.dbLots,
+      park.tpLots,
+      park.pmLots,
+      park.parkOwned,
+      park.avgRent,
+      park.rvLots,
+      park.rvLotRent,
+    ]);
   });
   console.log("array" + _values);
   const sheets = google.sheets({ version: "v4", auth });
@@ -134,32 +270,81 @@ async function updateSpreadsheet(auth, park) {
   });
 }
 
-/*
-(async () => {
-  var temp = await scrapeListing(
-    "https://www.mobilehomeparkstore.com/mobile-home-parks/2917796-1740-martin-luther-king-junior-boulevard-sebring-fl-33870-us-for-sale-in-sebring-fl",
-  );
-
-  authorize()
-    .then((auth) => updateSpreadsheet(auth, temp))
-    .catch(console.error);
-})();
-*/
-
-(async () => {
-  for (let i = 1; i < 9; i++) {
-    const pageLinks = await scrape(
-      `https://www.mobilehomeparkstore.com/mobile-home-parks-for-sale/usa/page/${i}`,
-    );
-    const parkResPromises = pageLinks.map((link) => scrapeListing(link));
-    var parkRes = await Promise.all(parkResPromises);
-    parkRes = parkRes.filter((res) => res !== null);
-
-    try {
-      const auth = await authorize();
-      await updateSpreadsheet(auth, parkRes);
-    } catch (error) {
-      console.error(error);
+function setParkData(park, parkData) {
+  parkData.forEach((parkData) => {
+    switch (parkData.desc) {
+      case "Occupancy:":
+        park.occupancy = parkData.data;
+        break;
+      case "Number of MH Lots:":
+        park.lots = parkData.data;
+        break;
+      case "Year Built:":
+        park.yearBuilt = parkData.data;
+        break;
+      case "Size:":
+        park.size = parkData.data;
+        break;
+      case "Average MH Lot Rent:":
+        park.lotRent = parkData.data;
+        break;
+      case "Community Type:":
+        park.communityType = parkData.data;
+        break;
+      case "Water:":
+        park.water = parkData.data;
+        break;
+      case "Water Paid By:":
+        park.waterPaid = parkData.data;
+        break;
+      case "Sewer:":
+        park.sewer = parkData.data;
+        break;
+      case "Gross Income:":
+        park.grossIncome = parkData.data;
+        break;
+      case "Operating Expense:":
+        park.operatingExpense = parkData.data;
+        break;
+      case "Net Operating Income:":
+        park.operatingIncome = parkData.data;
+        break;
+      case "Information Type:":
+        park.infoType = parkData.data;
+        break;
+      case "CAP Rate:":
+        park.capRate = parkData.data;
+        break;
+      case "Debt Information:":
+        park.debtInformation = parkData.data;
+        break;
+      case "Singlewide Lots:":
+        park.sgLots = parkData.data;
+        break;
+      case "Doublewide Lots:":
+        park.dbLots = parkData.data;
+        break;
+      case "Triplewide Lots:":
+        park.tpLots = parkData.data;
+        break;
+      case "Park Model Lots:":
+        park.pmLots = parkData.data;
+        break;
+      case "Number of Park-owned Homes:":
+        park.parkOwned = parkData.data;
+        break;
+      case "Average Home Rent:":
+        park.avgRent = parkData.data;
+        break;
+      case "Num of RV Lots:":
+        park.rvLots = parkData.data;
+        break;
+      case "Average RV Lot Rent:":
+        park.rvLotRent = parkData.data;
+        break;
+      default:
+        console.log(parkData.desc);
+        break;
     }
-  }
-})();
+  });
+}
